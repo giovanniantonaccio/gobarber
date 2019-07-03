def10da32a754db816a7458689f709eef040abec
Giovanni Antonaccio <giovanniantonaccio@MacBook-Pro-de-Giovanni.local>
2019-07-03 17:35:04 -0400

    Add project dependencies
    Adding ESLint, Prettier and EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ msg: 'hello world' });
-});
+routes.get('/', (req, res) => res.json({ msg: 'hello world' }));
 
 export default routes;
