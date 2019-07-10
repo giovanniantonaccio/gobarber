1819c764072ee5aa233db30a5802991a28e823b1
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-07-10 17:54:21 -0400

    Add provider list
    - Adding the Provider controller with the function to return the list of providers.
    - The index function return the information about the avatar associated with the provider.
    - It also uses the express.static function to serve the images.

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,13 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
@@ -19,5 +21,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
