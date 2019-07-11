aaf727d1d27ec29973156c6c3e924f349616f1f4
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-07-11 09:34:27 -0400

    Add appointment list for users and providers
    Adding the Appointment migration, model and controller. This is used for users to store appointments and retrieve a list with this info.
    Adding the ScheduleController, it is used for providers to show the list of his appointments during the day.
    Fixing the routes and updating some libraries.

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,11 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
-import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,8 +22,13 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
