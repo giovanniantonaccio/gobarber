2d0e823c036e51d458c8f8c0aef10515f094ddfc
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-07-11 13:45:11 -0400

    Add notifications for new appointments
    - Adding support for NoSQL database with mongoDB and mongoose;
    - Creating a Notification Schema;
    - Adding function to create the notification into mongoDB;
    - Fixing a bug where the appointments where not being saved with the hourStart on the database.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -101,9 +103,28 @@ class AppointmentController {
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
+    /**
+     * Notify new appointment to provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM, 'às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
+      read: false,
+    });
+
+    /**
+     * Return json with the created appointment
+     */
     return res.json(appointment);
   }
 }
